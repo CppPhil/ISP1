040a8edd5ac45b651ce5cc222d2e0a6cab27282e
CppPhil <CppPhil@users.noreply.github.com>
2019-04-20 01:56:11 +0200

    update

---
+++ include/romania.hpp
@@ -0,0 +1,28 @@
+/*!
+ * \file romania.hpp
+ * \brief Exports utilities to work with the romania example.
+ **/
+#pragma once
+#include <cstdint>     // uint64_t
+#include <graph_t.hpp> // graph_t
+#include <namespaces.hpp>
+#include <romanian_city.hpp> // romanian_city
+#include <unordered_map>     // unordered_map
+
+/*!
+ * \brief Creates a graph of the map of Romania.
+ * \return The resulting graph.
+ **/
+graph_t<romanian_city, cost, UNDIRECTED> create_romania_map();
+
+/*!
+ * \brief Returns the heuristic distance (h) for the given city
+ *        to the goal city (Bucharest).
+ * \return The h value for city.
+ **/
+uint64_t romania_heuristic(romanian_city city);
+
+/*!
+ * \brief Constant for the goal_city (Bucharest).
+ **/
+const romanian_city goal_city = romanian_city::Bucharest;
