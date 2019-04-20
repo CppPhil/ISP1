c4152bde454207bcdb0426272e2a1b7a4d151d90
CppPhil <CppPhil@users.noreply.github.com>
2019-04-20 21:44:19 +0200

    update

--- include/edges_of.hpp
+++ include/edges_of.hpp
@@ -1,6 +1,43 @@
 #pragma once
 #include <graph_t.hpp> // graph_t
-#include <stdexcept>   // logic_error
+#include <namespaces.hpp>
+#include <stdexcept> // logic_error
+
+/*!
+ * \brief Implementation function template to get the edges of a node in
+ *        a directed graph.
+ * \param node The node to get the outgoing edges of.
+ * \param graph The graph that contains the node.
+ * \tparam NodeIdentifier The type used to identify a node.
+ * \tparam CostType The cost type for the g values in the graph.
+ * \return A vector containing the outgoing edges of the node given.
+ **/
+template<typename NodeIdentifier, typename CostType>
+vector<typename graph_t<NodeIdentifier, CostType, DIRECTED>::node::edge>
+edges_of_impl(
+    NodeIdentifier                                     node,
+    const graph_t<NodeIdentifier, CostType, DIRECTED>& graph)
+{
+    return graph.get_out_edges(node);
+}
+
+/*!
+ * \brief Implementation function template to get the edges of a node in
+ *        an undirected graph.
+ * \param node The node to get the edges of.
+ * \param graph The graph that contains the node.
+ * \tparam NodeIdentifier The type used to identify a node.
+ * \tparam CostType The cost type for the g values in the graph.
+ * \return A vector containing the edges of the node given.
+ **/
+template<typename NodeIdentifier, typename CostType>
+vector<typename graph_t<NodeIdentifier, CostType, UNDIRECTED>::node::edge>
+edges_of_impl(
+    NodeIdentifier                                       node,
+    const graph_t<NodeIdentifier, CostType, UNDIRECTED>& graph)
+{
+    return graph.get_edges(node);
+}
 
 /*!
  * \brief Fetches the edges of a given node.
@@ -10,23 +47,11 @@
  * \tparam CostType The type used for the g cost values within the graph.
  * \tparam Nat The nature of the graph (directed / undirected)
  * \return A vector containing the edges of the node given.
- * \throws logic_error if the nature of the graph was not valid.
  **/
 template<typename NodeIdentifier, typename CostType, Nature Nat>
 vector<typename graph_t<NodeIdentifier, CostType, Nat>::node::edge> edges_of(
     NodeIdentifier                                node,
     const graph_t<NodeIdentifier, CostType, Nat>& graph)
 {
-    // Compile time branches to handle directed and undirected graphs.
-    // This way the compiler will only attempt to compile the branches
-    // that would actually compile.
-
-    // Nodes within directed graphs have 'out_edges'.
-    if constexpr (Nat == DIRECTED) { return graph.get_out_edges(node); }
-    else if constexpr (Nat == UNDIRECTED) { // Undirected case.
-        return graph.get_edges(node);
-    }
-    else { // Sanity check.
-        throw logic_error("Invalid nature of graph in edges_of!");
-    }
+    return edges_of_impl(node, graph);
 }
