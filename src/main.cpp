77b8440cb8e4a0f9941bd8815d02e26531b1952f
CppPhil <CppPhil@users.noreply.github.com>
2019-04-20 03:16:14 +0200

    timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,48 @@
 #include <a_star.hpp>    // a_star
 #include <pl/except.hpp> // handle_exceptions
+#include <pl/timer.hpp>  // timer
 #include <romania.hpp>   // create_romania_map, romania_heuristic
 
 int main()
 {
     try {
+        timer timer;
+
         const graph_t<romanian_city, cost, UNDIRECTED> romania_map
             = create_romania_map();
 
         const romanian_city start_city = romanian_city::Arad;
 
-        const path<romanian_city> a_star_path = a_star(
-            romania_map,
-            start_city,
-            [](romanian_city city) { return city == goal_city; },
-            &romania_heuristic);
+        const auto is_goal_city
+            = [](romanian_city city) { return city == goal_city; };
+
+        timer.reset();
+        const path<romanian_city> a_star_path
+            = a_star(romania_map, start_city, is_goal_city, &romania_heuristic);
+        const chrono::steady_clock::duration a_star_romania_time_taken
+            = timer.elapsed_time();
 
         cout << "\nCalculation of the shortest path from '" << start_city
              << "' to '" << goal_city << "':\n"
              << "A* total cost: " << a_star_path.g() << "\n\n"
-             << "path: " << a_star_path << '\n';
+             << "path: " << a_star_path << '\n'
+             << "time taken: "
+             << chrono::duration_cast<chrono::nanoseconds>(
+                    a_star_romania_time_taken)
+                    .count()
+             << "ns\n";
+
+        timer.reset();
+        a_star(romania_map, start_city, is_goal_city, [](romanian_city) {
+            return 0;
+        });
+        const chrono::steady_clock::duration a_star_romania_dijkstra_time_taken
+            = timer.elapsed_time();
+        cout << "dijkstra time taken: "
+             << chrono::duration_cast<chrono::nanoseconds>(
+                    a_star_romania_dijkstra_time_taken)
+                    .count()
+             << "ns\n";
 
         // non monotonic heuristic
         graph_t<string, cost, DIRECTED> g;
