9b7c5726e58d5dc8e70b9b44f973a4d4df339815
CppPhil <CppPhil@users.noreply.github.com>
2019-04-20 02:53:51 +0200

    getting rid of the closed list seems to have fixed the non-monotonic heuristic crap; while it would pick a non-optimal path before with the closed list present.

--- src/main.cpp
+++ src/main.cpp
@@ -18,4 +18,42 @@ int main()
          << "' to '" << goal_city << "':\n"
          << "A* total cost: " << a_star_path.g() << "\n\n"
          << "path: " << a_star_path << '\n';
+
+    // non monotonic heuristic
+    graph_t<string, cost, DIRECTED> g;
+    g("START", "B") = 5;
+    g("START", "C") = 5;
+    g("B", "D")     = 6;
+    g("C", "D")     = 5;
+    g("D", "E")     = 5;
+    g("E", "GOAL")  = 5;
+
+    const auto heuristic = [](string node) -> cost {
+        if (node == "START") { return 9; }
+        else if (node == "B") {
+            return 7;
+        }
+        else if (node == "C") {
+            return 8;
+        }
+        else if (node == "D") {
+            return 1;
+        }
+        else if (node == "E") {
+            return 1;
+        }
+        else if (node == "GOAL") {
+            return 0;
+        }
+
+        return numeric_limits<cost>::max();
+    };
+
+    const string start_node = "START";
+
+    const path<string> p = a_star(
+        g, start_node, [](string str) { return str == "GOAL"; }, heuristic);
+
+    cout << "\ntotal cost: " << p.g();
+    cout << "\np: " << p << '\n';
 }
