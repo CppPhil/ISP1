293dc5bf69a759d6094f0831f1be69aa2cadc46e
CppPhil <CppPhil@users.noreply.github.com>
2019-04-28 04:22:39 +0200

    Documentation added

--- include/grid_world.hpp
+++ include/grid_world.hpp
@@ -1,8 +1,14 @@
+/*!
+ * \file grid_world.hpp
+ * \brief Exports the create_grid_world function.
+ **/
 #pragma once
 #include <grid.hpp> // isp1::grid
 
-// TODO: Documentation
-
 namespace isp1 {
+/*!
+ * \brief Creates the grid world.
+ * \return The resulting grid.
+ **/
 grid create_grid_world();
 } // namespace isp1
